5622c0f400bd7f41d55e0feacbdf0d0cd4becee1
Humakt83 <urlact@yahoo.com>
2017-09-23 11:51:40 +0300

    Drag sort works

--- listmaker.js
+++ listmaker.js
@@ -1,28 +1,72 @@
-const listItems = [];
+var listItems = [];
+var draggingIndex = -1;
+
+const setDraggingIndex = (event) => {
+    draggingIndex = findMatchingIndex(event);
+};
 
 const addListItem = (item) => {
     const id = document.getElementsByClassName('listItem').length + 1;
-    listItems.push({id: id, item: item});
-    const element = document.createElement('div');
-    element.className = 'listItem';
-    element.innerHTML = `<span class="elementId">${id}.</span> ${item} <div class="deleteItem" onclick="deleteThis(${id})">x</div>`;
-    element.draggable = true;
+    const listItem = {id: id, item: item};
+    listItems.push(listItem);
+    const element = createElement(listItem);
     document.getElementById('listItems').appendChild(element);
 };
 
+const createElement = (item) => {
+    const element = document.createElement('div');
+    element.className = 'listItem';
+    element.innerHTML = `<span class="elementId">${item.id}.</span> ${item.item} <div class="deleteItem" onclick="deleteThis(${item.id})">x</div>`;
+    element.draggable = true;
+    element.addEventListener('dragstart', setDraggingIndex);
+    return element;
+}
+
 const deleteThis = (itemIndex) => {
-    const elementToRemove = document.getElementsByClassName('listItem')[itemIndex -1];
-    document.getElementById('listItems').removeChild(elementToRemove);
+    listItems = listItems.filter(val => val.id !== itemIndex);
+    listItems.forEach((val ,index) => val.id = index + 1);
     updateList();
 };
 
 const updateList = () => {
-    const listElements = document.getElementsByClassName('listItem');
-    [].forEach.call(listElements, (element, index) => {        
-        const id = index + 1;
-        element.innerHTML = `${id} <div class="deleteItem" onclick="deleteThis(${id})">x</div>`;        
+    const listParent = document.getElementById('listItems');
+    listParent.innerHTML = '';
+    listItems.forEach((item, index) => {
+        listParent.appendChild(createElement(item));       
     });
 };
+
+const dropAndSort = (event) => {
+    const matchingIndex = findMatchingIndex(event);
+    if (matchingIndex !== draggingIndex) {
+        const draggedItem = listItems.find(item => item.id === draggingIndex);
+        let items = listItems.filter(item => item.id !== draggingIndex);
+        draggedItem.id = matchingIndex;
+        items.forEach((item, index) => { 
+            if ((index + 1) >= matchingIndex) {
+                item.id = index + 2;
+            } else {
+                item.id = index + 1;
+            }
+        });
+        items.push(draggedItem)
+        items.sort((a, b) => a.id - b.id);
+        listItems = items;
+        updateList();
+    }
+};
+
+const findMatchingIndex = (event) => {
+    const elementPositions = [].map.call(document.getElementsByClassName('listItem'), (element) => element.getBoundingClientRect().top);
+    const mouseYPosition = event.clientY;
+    let matchingIndex = 0;
+    elementPositions.forEach((position, index) => matchingIndex = position > mouseYPosition ? matchingIndex : index);
+    return matchingIndex + 1;
+}
+
+const allowDrop = (event) => {
+    event.preventDefault();
+}
 
 document.getElementById('addItemToList').addEventListener('click', () => {
     addListItem(document.getElementById('itemInput').value);
