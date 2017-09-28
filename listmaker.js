763068f3adb1f70b48cd4f2c74429565764e536e
Humakt83 <urlact@yahoo.com>
2017-09-29 00:38:38 +0300

    Add option to paste list

--- listmaker.js
+++ listmaker.js
@@ -111,3 +111,29 @@ const showListAsText = () => {
     const text = listItems.reduce((previous, current) => previous + `<br>${current.id}. ${current.item}`, '');
     document.getElementById('textList').innerHTML = text;
 }
+
+const openPaste = () => {
+    document.getElementById('curtain').style = 'display: block';
+}
+
+const closeCurtain = () => {
+    document.getElementById('curtain').style = 'display: none';
+}
+
+const addPasteList = (stripNumbers) => {
+    const listContent = document.getElementById('pasteArea').value;
+    const list = listContent.split('\n');
+    let idToUse = listItems.length > 0 ? listItems[listItems.length - 1].id + 1 : 1;
+    for (item of list) {
+        if (stripNumbers && item.includes('.')) {
+            item = item.split('.').reduce((previous, current, index) => index <= 1 ? current : previous + '.' + current);
+        }
+        if (item.trim().length > 0) {
+            listItems.push({id: idToUse, item: item});
+            idToUse++;
+        }
+    }
+    updateList();
+    document.getElementById('pasteArea').value = '';
+    closeCurtain();
+}
